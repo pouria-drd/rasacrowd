da995af34e09e8c0b679dea9b5ac83257551ee20
thaeed <mr_saeed_da@yahoo.com>
2024-03-11 12:39:35 +0330

    site address length fix

--- src/pages/survey/service/forms/ContactForm.tsx
+++ src/pages/survey/service/forms/ContactForm.tsx
@@ -1,31 +1,39 @@
-import SectionTitle from "../../../../components/SectionTitle";
-import Input from "../../../../components/custom-ui/input/Input";
-import { ServiceFormProps, handleOnServiceDataChange } from "../../../../utils/formUtils";
-
+import SectionTitle from '../../../../components/SectionTitle';
+import Input from '../../../../components/custom-ui/input/Input';
+import { ServiceFormProps, handleOnServiceDataChange } from '../../../../utils/formUtils';
 
 function ContactForm(props: ServiceFormProps) {
     return (
         <>
             <SectionTitle title="راه های ارتباطی" />
 
-            <Input placeHolder="شماره همراه" maxLength={11}
+            <Input
+                placeHolder="شماره همراه"
+                maxLength={11}
                 defaultValue={props.data.agentPhoneNumber}
-                onChange={(e) => handleOnServiceDataChange(props, 'agentPhoneNumber', e.target.value)} />
+                onChange={(e) => handleOnServiceDataChange(props, 'agentPhoneNumber', e.target.value)}
+            />
 
-            <Input placeHolder="نشانی"
+            <Input
+                placeHolder="نشانی"
                 defaultValue={props.data.address}
                 onChange={(e) => handleOnServiceDataChange(props, 'address', e.target.value)}
             />
 
-            <Input placeHolder="نشانی اینترنتی(اختیاری)" maxLength={11}
+            <Input
+                placeHolder="نشانی اینترنتی (اختیاری)"
+                maxLength={40}
                 defaultValue={props.data.site}
-                onChange={(e) => handleOnServiceDataChange(props, 'site', e.target.value)} />
+                onChange={(e) => handleOnServiceDataChange(props, 'site', e.target.value)}
+            />
 
-            <Input placeHolder="ایمیل (اختیاری)"
+            <Input
+                placeHolder="ایمیل (اختیاری)"
                 defaultValue={props.data.email}
                 onChange={(e) => handleOnServiceDataChange(props, 'email', e.target.value)}
             />
-        </>)
+        </>
+    );
 }
 
 export default ContactForm;
