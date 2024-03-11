ece6b870e5b99737cc77376573c89226029aeb0c
thaeed <mr_saeed_da@yahoo.com>
2024-03-11 12:34:53 +0330

    serivce categories added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,44 @@
-import Supporters from "../components/Supporters";
-import SurveyLinkCard from "../components/SurveyLinkCard";
-import Banner from "../components/custom-ui/banner/Banner";
-import BaseCard from "../components/custom-ui/card/BaseCard";
-import { ROUTES } from "../router/routes";
+import Supporters from '../components/Supporters';
+import SurveyLinkCard from '../components/SurveyLinkCard';
+import Banner from '../components/custom-ui/banner/Banner';
+import BaseCard from '../components/custom-ui/card/BaseCard';
+import { ROUTES } from '../router/routes';
 
 function HomePage() {
     return (
         <BaseCard>
             <Banner />
 
-            <SurveyLinkCard title="پرسشنامه فردی" href={ROUTES.PERSONAL} badgeColor="green"
-                text="افرادی که ایده ای را راه اندازی کرده یا قصد راه اندازی آن را دارند." />
+            <SurveyLinkCard
+                title="پرسشنامه فردی"
+                href={ROUTES.PERSONAL}
+                badgeColor="green"
+                text="افرادی که ایده ای را راه اندازی کرده یا قصد راه اندازی آن را دارند."
+            />
 
-            <SurveyLinkCard title="پرسشنامه شرکت ها" href={ROUTES.TEAM} badgeColor="blue"
-                text="شرکت یا استارتاپ هایی که قصد توسعه محصولات یا خدمات خود را دارند." />
+            <SurveyLinkCard
+                title="پرسشنامه شرکت ها"
+                href={ROUTES.TEAM}
+                badgeColor="blue"
+                text="شرکت یا استارتاپ هایی که قصد توسعه محصولات یا خدمات خود را دارند."
+            />
 
-            <SurveyLinkCard title="پرسشنامه ادارات" href="/survey/organization" badgeColor="orange"
-                text="ادارات و سازمان های دولتی که طرحی برای فرصت های سرمایه گذاری دارند." />
+            <SurveyLinkCard
+                title="پرسشنامه ادارات"
+                href="/survey/organization"
+                badgeColor="orange"
+                text="ادارات و سازمان های دولتی که طرحی برای فرصت های سرمایه گذاری دارند."
+            />
 
-            <SurveyLinkCard title="پرسشنامه خدمات" href={ROUTES.SERVICE} badgeColor="green"
-                text="خدماتی که قصد توسعه آن را دارید." />
+            <SurveyLinkCard
+                title="پرسشنامه خدمات"
+                href={ROUTES.SERVICE}
+                badgeColor="green"
+                text="کسب و کارهایی که میتوانند خدماتی را به کسب و کارهای دیگر ارائه دهند."
+            />
             <Supporters />
         </BaseCard>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
--- src/pages/survey/service/forms/ServiceInformation.tsx
+++ src/pages/survey/service/forms/ServiceInformation.tsx
@@ -1,33 +1,41 @@
-import SectionTitle from "../../../../components/SectionTitle";
-import Input from "../../../../components/custom-ui/input/Input";
-import TextArea from "../../../../components/custom-ui/input/TextArea";
-import Dropdown from "../../../../components/custom-ui/dropdown/Dropdown";
-import { ServiceFormProps, handleOnServiceDataChange } from "../../../../utils/formUtils";
-
+import SectionTitle from '../../../../components/SectionTitle';
+import Input from '../../../../components/custom-ui/input/Input';
+import TextArea from '../../../../components/custom-ui/input/TextArea';
+import Dropdown from '../../../../components/custom-ui/dropdown/Dropdown';
+import { ServiceFormProps, handleOnServiceDataChange } from '../../../../utils/formUtils';
 
 function ServiceInformation(props: ServiceFormProps) {
     const categoryOptions: OptionProps[] = [
-        { label: 'طراحی سایت', value: 'طراحی سایت' },
-        { label: 'حساب داری', value: 'حساب داری' },
-    ]
+        { label: 'طراحی و دیزاین', value: 'طراحی و دیزاین' },
+        { label: 'حسابداری و حسابرسی', value: 'حسابداری و حسابرسی' },
+        { label: 'سایت و نرم افزار', value: 'سایت و نرم افزار' },
+        { label: 'سایر', value: 'سایر' },
+    ];
 
     return (
         <>
             <SectionTitle title="اطلاعات کلی" />
 
-            <Input placeHolder="عنوان کسب و کار خدماتی "
+            <Input
+                placeHolder="عنوان کسب و کار خدماتی "
                 defaultValue={props.data.serviceTitle}
-                onChange={(e) => handleOnServiceDataChange(props, 'serviceTitle', e.target.value)} />
+                onChange={(e) => handleOnServiceDataChange(props, 'serviceTitle', e.target.value)}
+            />
 
-            <Dropdown title="نوع خدمت" options={categoryOptions}
+            <Dropdown
+                title="نوع خدمت"
+                options={categoryOptions}
                 defaultValue={props.data.category}
-                onSelectOption={(e) => handleOnServiceDataChange(props, 'category', e.label)} />
+                onSelectOption={(e) => handleOnServiceDataChange(props, 'category', e.label)}
+            />
 
-            <TextArea placeHolder="شرح خدمت"
+            <TextArea
+                placeHolder="شرح خدمت"
                 defaultValue={props.data.description}
                 onChange={(e) => handleOnServiceDataChange(props, 'description', e.target.value)}
             />
-        </>)
+        </>
+    );
 }
 
 export default ServiceInformation;
